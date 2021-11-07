cab10f7feec4aa5ddd8168ec731367921ca52c13
itamarerenberg <59229663+itamarerenberg@users.noreply.github.com>
2021-11-07 14:42:42 +0200

    creating pl, bl, dal, be projects

---
+++ BL/FlowersFinder.cs
@@ -0,0 +1,24 @@
+﻿using BE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class FlowersFinder
+    {
+        private FlowersFinder Dal;
+
+        public FlowersFinder()
+        {
+            Dal = new FlowersFinder();
+        }
+
+        public List<Flower> Search(string str)
+        {
+            return null;
+        }
+    }
+}
---
+++ dal/FlowersReader.cs
@@ -0,0 +1,35 @@
+﻿using BE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dal
+{
+    public class FlowersReader
+    {
+        public List<Flower> flowers { get; set; }
+        public FlowersReader()
+        {
+            flowers = new List<Flower> {
+                new Flower
+                {
+                    Color = "red",
+                    Description = "beutifull",
+                    Location = "Israel",
+                    Name="iris",
+                    Image= @"https://ichef.bbci.co.uk/news/976/cpsprodpb/C448/production/_117684205_lotus.jpg"
+                },
+                new Flower
+                {
+                    Color = "green",
+                    Description = "lovely",
+                    Location = "USA",
+                    Name="shosh",
+                    Image= @"https://ichef.bbci.co.uk/news/976/cpsprodpb/C448/production/_117684205_lotus.jpg"
+                }
+            };
+        }
+    }
+}
